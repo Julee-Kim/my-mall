84579a8aa10d610a2aeea07422d899d40696f0c8
Julee-Kim <jlkim0323@kakao.com>
2024-05-20 13:59:38 +0900

    Refactor: Modal - children 타입 검사 조건문 추가

--- components/_common/modal/ModalHeader.tsx
+++ components/_common/modal/ModalHeader.tsx
@@ -5,16 +5,24 @@ import styles from '@/components/_common/modal/Modal.module.scss'
 const ModalHeader = ({ children }: { children?: ReactNode }) => {
   const { onCancel, useClose } = useModalContext()
 
-  return (
-    <div className={styles.modalHeader}>
-      <h3>{children}</h3>
-      {useClose ? (
-        <button className={styles.btnClose} onClick={onCancel}>
-          x
-        </button>
-      ) : null}
-    </div>
-  )
+  let headerContent = null
+
+  if (typeof children === 'string') {
+    headerContent = (
+      <div className={styles.modalHeader}>
+        <h3>{children}</h3>
+        {useClose ? (
+          <button className={styles.btnClose} onClick={onCancel}>
+            x
+          </button>
+        ) : null}
+      </div>
+    )
+  } else {
+    headerContent = children
+  }
+
+  return headerContent
 }
 
 export default ModalHeader
