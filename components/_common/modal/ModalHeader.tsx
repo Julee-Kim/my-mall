87d37c5a832fcd92d9215cc7e9f3a368aa1882e9
Julee-Kim <jlkim0323@kakao.com>
2024-05-28 09:06:55 +0900

    Refactor: 모달 컴포넌트 - 헤더 children 문자만 받도록 변경, 닫기 아이콘 추가

--- components/_common/modal/ModalHeader.tsx
+++ components/_common/modal/ModalHeader.tsx
@@ -1,28 +1,21 @@
-import { ReactNode } from 'react'
+import React from 'react'
+import { IoCloseOutline } from 'react-icons/io5'
 import { useModalContext } from '@/components/_common/modal/Modal'
 import styles from '@/components/_common/modal/Modal.module.scss'
 
-const ModalHeader = ({ children }: { children?: ReactNode }) => {
+const ModalHeader = ({ children }: { children?: string }) => {
   const { onCancel, useClose } = useModalContext()
 
-  let headerContent = null
-
-  if (typeof children === 'string') {
-    headerContent = (
-      <div className={styles.modalHeader}>
-        <h3>{children}</h3>
-        {useClose ? (
-          <button className={styles.btnClose} onClick={onCancel}>
-            x
-          </button>
-        ) : null}
-      </div>
-    )
-  } else {
-    headerContent = children
-  }
-
-  return headerContent
+  return (
+    <div className={styles.modalHeader}>
+      <h3>{children}</h3>
+      {useClose && (
+        <button className={styles.btnClose} onClick={onCancel}>
+          <IoCloseOutline size={20} />
+        </button>
+      )}
+    </div>
+  )
 }
 
 export default ModalHeader
